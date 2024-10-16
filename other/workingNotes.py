352621acd3c24299e78d2d17fbcbc9de2787d283
Felix <investment.felixlam@gmail.com>
2024-10-16 20:26:03 +0800

    initialize phoebe for bybit

---
+++ other/workingNotes.py
@@ -0,0 +1,16 @@
+# working Notes
+
+"""
+main
+1) refactor glassnode
+- init phoebe for bybit
+- init product_list -> ["usdm", "coinm"] and ["linear", "inverse"]
+- fix all strategy_type/ model for all loop issue
+- symbol_list for binance (_PERP for binance_symbol_list coinm)
+
+waiting team
+1) fixing websocket issue -> waiting reply
+2) change to notional value instead of base_qty -> will do so in the next version
+3) okex celesty -> waiting team help on getting funding rate
+4) refactor/ restructure trading system
+"""
