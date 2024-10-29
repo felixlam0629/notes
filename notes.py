a22463fe086bfaa314e2c81817b9a141df7b6572
Felix <investment.felixlam@gmail.com>
2024-10-29 21:00:25 +0800

    remove useless notes

--- notes.py
+++ notes.py
@@ -24,32 +24,4 @@ while latestDB < bitmexLatest:
     writeToDB(r)
     latestDB = r[-1]["timestamp"]
     time.sleep(1)
-
-
-
-# Define bug handling
-class ValueTooHighError(Exception):
-    pass
-
-class ValueTooSmallError(Exception):
-    def __init__(self, message, value):
-        self.message = message
-        self.value   = value
-
-def test_value(x):
-    if x > 100:
-        raise ValueTooHighError("value is too high")
-
-    if x < 5:
-        raise ValueTooSmallError("value is too small", x)
-
-try:
-    test_value(0)
-
-except ValueTooHighError as e:
-    print(e)
-
-except ValueTooSmallError as e:
-    print(e.message, e.value)
-
 '''
