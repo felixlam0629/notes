f390686685c348995d6fb6b6602044436dce2a39
Felix <investment.felixlam@gmail.com>
2024-11-27 22:22:02 +0800

    update workingNotes and strategyNotes

--- other/workingNotes.py
+++ other/workingNotes.py
@@ -2,33 +2,39 @@
 
 """
 main
-1) transfer bybit fund to binance 4th sub-acc for phoebe (waiting team onboard 4th acc)
+1) order_history
+- order_times -> how many times get cancelled -> filled
+- min, max and avg order_times
+* seems cannot find cancelled order (GTX order hit market order stuffs)
+* be careful with manual order adjustment
+
+2) trade history
+- order_slippage (trade_price - theo_price)
+* be careful with manual order adjustment
+
+3) result_df last column
+- some data do not finish its resolution (kline, volatility index, ...)
+- therefore, the last column should be eliminated
+- be careful, if last_column deleted -> not latest_data -> cant get latset backtest_df for positioning
+
+4) data optimization
+- if data smaller than 3-year history -> do not download and skip
+
+5) (glassnode data) - del data for useless symbol and exchange_list as well
 --------------------------------------------------
 1) optimize current strategy (celesty, claire)
 - celesty + claire (resampled to 8hrs) -> if (index + funding) > futures -> long/ short
+- claire - try 12h-resolution close position
 
-2) big backtest on the following strategy
-- phoebe -> updating all data
+2) big backtest on phoebe -> running
 --------------------------------------------------
-1) claire - try 12h-resolution close position
-
-2) deribit data scripts
-problem: data is not trustable to use atm LMAO
-
-3) backtest_df data visualization
-- visualize data with time -> check data quality
-
-4) backtest analysis
-- pnl (long, short) distribution -> check if one-off -> affecting the whole result
-- monthly pnl distribution -> check if one-off
-
-5) monthly/ daily pnl analysis
-
-
-team
-1) change to notional value instead of base_qty -> will do so in the next version
-2) adjust notional to aware of MDD of each strategy (some > 100%...)
-2) refactor/ restructure trading system -> waiting okex onboard
+waiting team
+1) copy SingleResult from server to local   -> waiting backend get ready
+2) deploy celesty for binance coinm_futures -> waiting tbackend get ready
+3) copy trading repo from tc to local       -> waiting tbackend get ready
+4) adjust notional to aware of MDD of each strategy -> waiting backend get ready
+5) calculate exchange/ total performance -> waiting main acc details
+6) short-cut = ctrl + b + [
 --------------------------------------------------
 other (data)
 1) get_data for dataScrapper
@@ -38,22 +44,6 @@ other (data)
 - 4.1) coinm_futures with contractType
 - 4.2) funding_rate upper/ lower limit from exchange API
 - 4.3) N.A for data
---------------------------------------------------
-other (backtest)
-3) backtest_df data visualization
-- visualize data with time -> check data quality
-
-4) backtest analysis
-- pnl (long, short) distribution -> check if one-off -> affecting the whole result
-- monthly pnl distribution -> check if one-off
---------------------------------------------------
-other (analysis)
-1) post-trade analysis (from exchange UI)
-- order -> 1) order_time (delay), 2) order_slippage
-- trade -> 1) trdae_time (waiting), 2) trade_slippaage
-- performace -> 1) each exchange, 2) each account, 3) each strategy, 4) each symbol
-
-2) post-trade analysis from UI
 --------------------------------------------------
 optimization (backtest)
 1. magnitude calculation method
@@ -84,10 +74,10 @@ thought
 
 5. bvol analysis (waiting answer from calvin)
 --------------------------------------------------
-other (trading)
-1) SingleResult function
-- getdata from exchange
-- calculate trading signal
-- show in a df for real- trade decision
+other (analysis)
+1) finalize pnlAnalyzer
 --------------------------------------------------
+other (team)
+1) change to notional value instead of base_qty -> will do so in the next version
+2) refactor/ restructure trading system -> waiting okex onboard
 """
