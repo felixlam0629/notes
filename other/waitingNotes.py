0df48b6dee689b99cbec794bc1d16f390a4d7f9d
Felix <investment.felixlam@gmail.com>
2025-01-13 14:32:26 +0800

    update workingNotes and waitingNotes

--- other/waitingNotes.py
+++ other/waitingNotes.py
@@ -25,36 +25,6 @@ rationale: if both call iv increases and volume sudden increaded -> most of them
 rationale: if call iv increases, volume did not changed          -> market maker increases iv -> some of them might be institutional investors
 - 3) iv * put_call ratio
 
-2. funding_rate with multi- factor (glassnode)
-- as funding_rate is testified to be a profitable strategy
-- therefore, focos on magnitude calculation method with
-
-- BTC, ETH, DOGE, AVAX, GRT only
-- funding_rate * open_interest/ volume? maximize the retail effect on funding_rate/ position
-- funding_rate * estimated leverage ratio?
-
-3. how to use bvol?
-- realized that when the market does not move/ boring -> funding_rate as well
-- no movement in funding_rate -> no signal from celesty
--> higher bvol -> start celesty?
--> lower bvol -> momentum strategy~~?
-
-thought
-1) lower volatility -> less retail investor participation
-2) higher volatility -> more retail investors -> alpha for CTA
-
-4. bvol analysis (waiting answer from calvin)
-
-5. single para_dict vs full_para_dict vs all_para_combination in backtestEngine
+2. single para_dict vs full_para_dict vs all_para_combination in backtestEngine
 --------------------------------------------------
-to-do list (correlation)
-1) optimize correlationChecker
-- by symbol
-- by single symbol in single strategy
---------------------------------------------------
-to-do list (analysis)
-1) finalize pnlAnalyzer
---------------------------------------------------
-to-do list (team)
-2) refactor/ restructure trading system -> waiting dex onboard
 """
--- other/workingNotes.py
+++ other/workingNotes.py
@@ -2,28 +2,23 @@
 
 """
 main
+# test claire different resolution
+
 top
-1) update celestyParams in backend -> waiting position closed
-2) optimize phoebe for next round backtesting -> running single
-2.1 resampling (method: past mean, past total, now_data) -> 12h, 8h, 6h, 4h, 2h, 1h
+1) Backtest glassnode
+1.1 resampling/ different resolution (method: past mean, past total, now_data) -> 12h, 8h, 6h, 4h, 2h, 1h
 
 now_data
 - backtested: 12h, 8h, 6h
 - running: 4h
 
-2.2 backtest diff model -> dig deep time series modelling
+1.2 different model
 * rmb Signal.py changed in non-24h backtesting
 
-2) clear all subscription passages (chin sum FI, econ_one)
 
-1) deploy 4th strategy on binance -> waiting dev team to add key to backend
-2) optimize phoebe to multi-factor trading -> waiting to ask how to deal with it later
-3) clear all subscription passage (patreon)
-4) test bybit api with my personal acc
-5) update symbol_list for glassnodeScrapper
+
+2) Backtest trading_idea
+2.1 price_data (sd, different resolution, momentum)
+2.3 taker buy/sell volume (sd, different resolution, momentum, reversion)
 --------------------------------------------------
-6000
-AVAX 660 (2000)
-DYDX 1000 (2000)
-SFP 1000 (2000)
 """
