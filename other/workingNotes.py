a1d93386ece940f77b135f22c7587a5edcbd4837
Felix <investment.felixlam@gmail.com>
2025-02-05 15:13:33 +0800

    update workingNotes

--- other/workingNotes.py
+++ other/workingNotes.py
@@ -1,39 +1,24 @@
 # working Notes
 
 """
+scale up strategy -> waiting momentum end to change params in christy_v1
+trade phoebe other factors
+
 main
-update phoebe script
-add christyResult
+1) backtest phoebe good endpoint_list
+2) trade phoebe good endpoint_list
 
-# optimize strategy model/ type params
-
-2.1 price_data (sd, different resolution, momentum only)
-- binance
-scrapping: /
-waiting: 5m, 15m, 30m, 1h, 2h, 4h
-backtesting:
-finished: 24h, 12h, 8h, 6h
-
-- bybit
-scrapping: 5m
-waiting: 1h, 30m, 15m
-backtesting:
-finished: 2h, 4h, 6h, 12h, 24h
-
-top
-1) Backtest glassnode
-1.1 resampling/ different resolution (method: past mean, past total, now_data) -> 12h, 8h, 6h, 4h, 2h, 1h
-
-now_data
-- backtested: 12h, 8h, 6h
-- running: 4h
-
-1.2 different model
+3) adopt different model to backtestEngine
 * rmb Signal.py changed in non-24h backtesting
 
+Optimization for 25Q1
+- Resampling
+- Modelling
 
-
-2) Backtest trading_idea
-2.3 taker buy/sell volume (sd, different resolution, momentum, reversion)
+waiting
+- glassnode resampling
+(method: past mean, past total, now_data) -> 12h, 8h, 6h, 4h, 2h, 1h
+- optimize phoebe current endpoint (resampling only)
+- backtest taker buy/sell volume (sd, different resolution, momentum, reversion)
 --------------------------------------------------
 """
