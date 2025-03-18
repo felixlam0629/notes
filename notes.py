d048b31c9c2d41f811218c5004142453cf34bf7f
Felix <investment.felixlam@gmail.com>
2025-03-18 18:57:39 +0800

    remove old and useless notes

--- notes.py
+++ notes.py
@@ -17,15 +17,4 @@ first_data_date = datetime.date(2012, 1, 3)                   = 2012-01-03
 first_data_date_int = int(first_data_date.strftime("%Y%m%d")) = 20120103
 
 print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + str(counter) + "//" + str(num_of_combination) +  " combination " + code + " " + freq + " data downloading...")
-
-import requests
-baseURI = "https://www.bitmex.com/api/v1"
-endpoint = "/trade/bucketed"
-while latestDB < bitmexLatest:
-    params = {"binSize": "1m", "symbol": symbol, "count" : 300, "startTime" : latestDB}
-    r = requests.get(baseURI + endpoint, params = params)
-    r = r.json()
-    writeToDB(r)
-    latestDB = r[-1]["timestamp"]
-    time.sleep(1)
 '''
